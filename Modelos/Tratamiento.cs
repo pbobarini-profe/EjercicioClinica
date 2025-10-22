a72641f88737dc448b5bf5cca66e3914da4b027e
PablissimoB <pablo.bobarini@gmail.com>
2025-10-22 15:51:00 -0300

    More Entities

---
+++ Modelos/Tratamiento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    public class Tratamiento
+    {
+        public int id { get; set; }
+        public DetallesHistoriasClinicas detalleHistoriaClinica { get; set; }
+        public Medicamentos medicamento { get; set; }
+        public decimal peso { get; set; }//graduacion del medicamento
+        public DateTime fechaInicio { get; set; }
+        public string descripcion { get; set; }
+        public TiposResultadosTratamientos resultado { get; set; }
+    }
+}
